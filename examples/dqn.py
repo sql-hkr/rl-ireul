130b549edea6587f291b361cb6da0843b1d38f4d
sql-hkr <sql.hkr@gmail.com>
2022-07-18 20:34:44 +0900

    Fix mini_batch_train

--- examples/dqn.py
+++ examples/dqn.py
@@ -4,13 +4,12 @@ from ireul.common.utils import mini_batch_train
 from ireul.dqn.dqn import DQNAgent
 
 env_id = "CartPole-v1"
-MAX_EPISODES = 1000
-MAX_STEPS = 500
+MAX_STEPS = 100000
 BATCH_SIZE = 32
 
 env = gym.make(env_id)
 agent = DQNAgent(env, use_conv=False)
-episode_rewards = mini_batch_train(env, agent, MAX_EPISODES, MAX_STEPS, BATCH_SIZE)
+episode_rewards = mini_batch_train(env, agent, MAX_STEPS, BATCH_SIZE)
 
 import numpy as np
 import matplotlib.pyplot as plt
--- ireul/common/utils.py
+++ ireul/common/utils.py
@@ -1,50 +1,42 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.autograd as autograd
-import numpy as np
-import math
 import gym
+from .base_class import BaseAgent
 
+def mini_batch_train(
+    env: gym.Env,
+    agent: BaseAgent,
+    max_steps: int,
+    batch_size: int
+    ):
 
-def mini_batch_train(env, agent, max_episodes, max_steps, batch_size):
     episode_rewards = []
-    train_done = False
 
-    for episode in range(max_episodes):
-        state = env.reset()
-        episode_reward = 0
+    state = env.reset()
+    episode_reward = 0
 
-        for step in range(max_steps):
-            action = agent.get_action(state)
-            next_state, reward, done, _ = env.step(action)
-            agent.replay_buffer.push(state, action, next_state, reward, done)
-            episode_reward += reward
+    for step in range(max_steps):
+        action = agent.get_action(state)
+        next_state, reward, done, _ = env.step(action)
+        agent.replay_buffer.push(state, action, next_state, reward, done)
+        episode_reward += reward
 
-            if len(agent.replay_buffer) > batch_size:
-                agent.update(batch_size)   
+        if len(agent.replay_buffer) > batch_size:
+            agent.update(batch_size)   
 
-            # <<< TODO: implement exactly
-            if step % 10 == 0:
-                agent.sync()
-            # >>>
+        if step % 10 == 0:
+            agent.target_net_sync()
 
-            if done or step == max_steps-1:
-
-                # <<< TODO: implement exactly
-                if episode_reward > 490:
-                    train_done = True
-                # >>>
-
-                episode_rewards.append(episode_reward)
-                print("Episode " + str(episode) + ": " + str(episode_reward))
+        if done or step == max_steps-1:
+            episode_rewards.append(episode_reward)
+            print("Episode " + str(len(episode_rewards)) + ": " + str(episode_reward))
+            
+            if episode_reward > 490:
                 break
 
-            state = next_state
+            # reset
+            state = env.reset()
+            episode_reward = 0
+            continue
 
-        # <<< TODO: implement exactly
-        if train_done:
-            break
-        # >>>
+        state = next_state
 
     return episode_rewards
