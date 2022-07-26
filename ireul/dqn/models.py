42a3fd5a2cc5db5aa2ff609d466305d283e9e782
_hkr <61460726+sql-hkr@users.noreply.github.com>
2022-07-26 13:09:45 +0900

    Create NoisyDQN

--- ireul/dqn/models.py
+++ ireul/dqn/models.py
@@ -1,8 +1,9 @@
 from torch import nn
+from ireul.common.utils import FactorizedNoisyLinear
 
 class DQN(nn.Module):
     def __init__(self, n_inputs, n_actions):
-        super(DQN, self).__init__()
+        super().__init__()
         
         self.layers = nn.Sequential(
             nn.Linear(n_inputs, 128),
@@ -12,3 +13,50 @@ class DQN(nn.Module):
         
     def forward(self, x):
         return self.layers(x)
+
+class NoisyDQN(nn.Module):
+
+    def __init__(self, n_inputs, n_actions):
+        super().__init__()
+
+        self.noisy_fc = nn.Sequential(
+            nn.Linear(n_inputs, 128),
+            nn.ReLU(),
+            FactorizedNoisyLinear(128, 128),
+            nn.ReLU(),
+            FactorizedNoisyLinear(128, n_actions)
+        )
+    
+    def forward(self, x):
+        return self.noisy_fc(x)
+
+class DuelingDQN(nn.Module):
+
+    def __init__(self, n_inputs, n_actions):
+        super().__init__()
+        
+        self.feauture_layer = nn.Sequential(
+            nn.Linear(n_inputs, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU()
+        )
+
+        self.value_stream = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+        self.advantage_stream = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_actions)
+        )
+
+    def forward(self, x):
+        features = self.feauture_layer(x)
+        values = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        return values + (advantages - advantages.mean())
+        
