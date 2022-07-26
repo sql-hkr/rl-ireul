a9d61bf433960de9a25c0adbef9a39e783033f17
_hkr <61460726+sql-hkr@users.noreply.github.com>
2022-07-26 13:14:23 +0900

    Add NoisyDQN and DuelingDQN

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -1,41 +1,37 @@
-import gym
-
-from ireul.common.utils import mini_batch_train
-from ireul.dqn.dqn import DQNAgent
-from ireul.dqn.ddqn import DDQNAgent
-
 from collections import namedtuple
 import copy
-
-env_id = "CartPole-v1"
-MAX_STEPS = 100000
-BATCH_SIZE = 32
-
-history = []
-for i in range(3):
-    env = gym.make(env_id)
-    agent = DDQNAgent(env, use_conv=False)
-    episode_rewards1 = mini_batch_train(env, agent, MAX_STEPS, BATCH_SIZE)
-
-    env = gym.make(env_id)
-    agent = DDQNAgent(env, use_conv=False)
-    episode_rewards2 = mini_batch_train(env, agent, MAX_STEPS, BATCH_SIZE)
-
-    history.append((
-        copy.deepcopy(episode_rewards1),
-        copy.deepcopy(episode_rewards2)
-    ))
-    
+from typing import Dict
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+import gym
 
-for indx, h in enumerate(history):
-    dqn = h[0]
-    ddqn = h[1]
-    plt.plot(np.arange(len(dqn)), np.array(dqn), label=f"alg1: #{indx}")
-    plt.plot(np.arange(len(ddqn)), np.array(ddqn), label=f"alg2: #{indx}")
+from ireul.common.utils import mini_batch_train
+from ireul.common.base_class import BaseAgent
+from ireul.dqn.dqn import DQNAgent
+from ireul.dqn.ddqn import DDQNAgent
+from ireul.dqn.noisy_dqn import NoisyDQNAgent
+from ireul.dqn.dueling_dqn import DuelingDQNAgent
+
+env_id = "CartPole-v1"
+MAX_STEPS = 100000
+BATCH_SIZE = 32
+LR = 1e-3
+
+agents: Dict[str, BaseAgent] = {
+    "DQN": DQNAgent,
+    "DDQN": DDQNAgent,
+    "NoisyDQN": NoisyDQNAgent,
+    "DuelingDQN": DuelingDQNAgent
+    }
+
+for name, agent in agents.items():
+    env = gym.make(env_id)
+    A = agent(env, use_conv=False, learning_rate=LR)
+    episode_rewards = mini_batch_train(env, A, MAX_STEPS, BATCH_SIZE)
+    plt.plot(np.arange(len(episode_rewards)), np.array(episode_rewards), label=name)
+
 plt.legend()
 plt.xlabel("eps")
 plt.ylabel("reward")
