6b6f5bd8af9fddc2df113f34e2d125ced9dc56c9
_hkr <61460726+sql-hkr@users.noreply.github.com>
2022-07-26 13:08:46 +0900

    Add NoisyLinear and FactorizedNoisyLinear

--- ireul/common/utils.py
+++ ireul/common/utils.py
@@ -1,7 +1,13 @@
 import gym
 from .base_class import BaseAgent
 
+import math
 from datetime import date, datetime
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
 from tensorboardX import SummaryWriter
 
 # TODO: implement a logger
@@ -50,3 +56,92 @@ def mini_batch_train(
     writer.close()
     
     return episode_rewards
+
+class NoisyLinear(nn.Module):
+
+    def __init__(self, num_in, num_out, is_training=True):
+        super(NoisyLinear, self).__init__()
+        self.num_in = num_in
+        self.num_out = num_out
+        self.is_training = is_training
+
+        self.mu_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.mu_bias = nn.Parameter(torch.FloatTensor(num_out))
+        self.sigma_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.sigma_bias = nn.Parameter(torch.FloatTensor(num_out))
+        self.register_buffer("epsilon_weight", torch.FloatTensor(num_out, num_in)) 
+        self.register_buffer("epsilon_bias", torch.FloatTensor(num_out))
+
+        self.reset_parameters()
+        self.reset_noise()
+
+    def forward(self, x):
+        self.reset_noise()
+
+        if self.is_training:
+            weight = self.mu_weight + self.sigma_weight * self.epsilon_weight
+            bias = self.mu_bias + self.sigma_bias * self.epsilon_bias
+        else:
+            weight = self.mu_weight
+            bias = self.mu_bias
+
+        return F.linear(x, weight, bias)
+
+    def reset_parameters(self):
+        std = math.sqrt(3 / self.num_in)
+        self.mu_weight.data.uniform_(-std, std)
+        self.mu_bias.data.uniform_(-std,std)
+
+        self.sigma_weight.data.fill_(0.017)
+        self.sigma_bias.data.fill_(0.017)
+
+    def reset_noise(self):
+        self.epsilon_weight.data.normal_()
+        self.epsilon_bias.data.normal_()
+
+    
+class FactorizedNoisyLinear(nn.Module):
+
+    def __init__(self, num_in, num_out, is_training=True):
+        super(FactorizedNoisyLinear, self).__init__()
+        self.num_in = num_in
+        self.num_out = num_out 
+        self.is_training = is_training
+
+        self.mu_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.mu_bias = nn.Parameter(torch.FloatTensor(num_out)) 
+        self.sigma_weight = nn.Parameter(torch.FloatTensor(num_out, num_in))
+        self.sigma_bias = nn.Parameter(torch.FloatTensor(num_out))
+        self.register_buffer("epsilon_i", torch.FloatTensor(num_in))
+        self.register_buffer("epsilon_j", torch.FloatTensor(num_out))
+
+        self.reset_parameters()
+        self.reset_noise()
+
+    def forward(self, x):
+        self.reset_noise()
+        
+        if self.is_training:
+            epsilon_weight = self.epsilon_j.ger(self.epsilon_i)
+            epsilon_bias = self.epsilon_j
+            weight = self.mu_weight + self.sigma_weight * epsilon_weight
+            bias = self.mu_bias + self.sigma_bias * epsilon_bias
+        else:
+            weight = self.mu_weight
+            bias = self.mu_bias
+
+        return F.linear(x, weight, bias)
+
+    def reset_parameters(self):
+        std = 1 / math.sqrt(self.num_in)
+        self.mu_weight.data.uniform_(-std, std)
+        self.mu_bias.data.uniform_(-std, std)
+
+        self.sigma_weight.data.fill_(0.5 / math.sqrt(self.num_in))
+        self.sigma_bias.data.fill_(0.5 / math.sqrt(self.num_in))
+
+    def reset_noise(self):
+        eps_i = torch.randn(self.num_in)
+        eps_j = torch.randn(self.num_out)
+        self.epsilon_i = eps_i.sign() * (eps_i.abs()).sqrt()
+        self.epsilon_j = eps_j.sign() * (eps_j.abs()).sqrt()
